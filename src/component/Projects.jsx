8778b623cc2f0eb3b0c209e7ce9983f1cc54f540
njandersen <njandersen22@gmail.com>
2022-08-31 10:37:53 -0400

    Made changes to make site more responsive

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -5,23 +5,25 @@ function Projects() {
     <>
       <section id="projects">
         <h1 className="projects-heading">Projects</h1>
-        <div className="card project1-card" data-Aos="fade-right">
-          <h2>Personal Blog Site</h2>
-          <img
-            className="project1-img"
-            src="../files/blog screenshot.png"
-            alt="project1"
-          />
-          <h3>
-            A simple Blog site built in Next.js and Firebase. It can be found at
-            the link below.
-          </h3>
-          <a
-            href="https://personal-blog-njandersen.vercel.app/"
-            className="project1-link"
-          >
-            Personal Blog
-          </a>
+        <div className="project-container">
+          <div className="card project1-card" data-Aos="fade-right">
+            <h2>Personal Blog Site</h2>
+            <img
+              className="project1-img"
+              src="../files/blog screenshot.png"
+              alt="project1"
+            />
+            <h3>
+              A simple Blog site built in Next.js and Firebase. It can be found
+              at the link below.
+            </h3>
+            <a
+              href="https://personal-blog-njandersen.vercel.app/"
+              className="project1-link"
+            >
+              Personal Blog
+            </a>
+          </div>
         </div>
       </section>
     </>
--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -9,32 +9,35 @@ function Skills() {
 
   return (
     <section id="skills">
-      <div className="skills-header" data-Aos="fade-down">
-        <h1>Skills & Experience</h1>
-        <p className="skills-about">
-          Ever since I started my computer science program in 2019, I've been on
-          a mission to learn as much as possible. It took me a while to finally
-          find a tech stack that I really and truly enjoyed working on. I
-          thought it was java. But eventually I landed on Javascript, more
-          specifically react. I've enjoyed building sites with various different
-          stacks and I would love an opportunity to show off my skills
-        </p>
-      </div>
-      <div className="card frontend-skills" data-Aos="fade-up">
-        <ul className="skills-list">
-          <h2>Front End Development</h2>
-          <li>React</li>
-          <li>Javascript</li>
-          <li>CSS/HTML</li>
-        </ul>
-      </div>
-      <div className="card backend-skills" data-Aos="fade-up">
-        <ul>
-          <h2>Back End Development</h2>
-          <li>Node.js</li>
-          <li>Next.js</li>
-          <li>Firebase</li>
-        </ul>
+      <div className="skills-container">
+        <div className="skills-header" data-Aos="fade-down">
+          <h1>Skills & Experience</h1>
+          <p className="skills-about">
+            Ever since I started my computer science program in 2019, I've been
+            on a mission to learn as much as possible. It took me a while to
+            finally find a tech stack that I really and truly enjoyed working
+            on. I thought it was java. But eventually I landed on Javascript,
+            more specifically react. I've enjoyed building sites with various
+            different stacks and I would love an opportunity to show off my
+            skills
+          </p>
+        </div>
+        <div className="card frontend-skills" data-Aos="fade-up">
+          <ul className="skills-list">
+            <h2>Front End Development</h2>
+            <li>React</li>
+            <li>Javascript</li>
+            <li>CSS/HTML</li>
+          </ul>
+        </div>
+        <div className="card backend-skills" data-Aos="fade-up">
+          <ul>
+            <h2>Back End Development</h2>
+            <li>Node.js</li>
+            <li>Next.js</li>
+            <li>Firebase</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
