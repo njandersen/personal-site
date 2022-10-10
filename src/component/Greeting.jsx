e8d99b7e926d2c5031810abebdc85eb7899c8c11
njandersen <njandersen22@gmail.com>
2022-10-10 12:29:47 -0400

    refactor of greeting section

--- src/component/Greeting.jsx
+++ src/component/Greeting.jsx
@@ -3,10 +3,18 @@ import React from "react";
 function Greeting() {
   return (
     <section id="greeting">
-      <div className="greetings">
-        <h3 className="hello">Hello There,</h3>
-        <h1 className="name">I'm N. J. Andersen.</h1>
-        <h3 className="tag">Full stack web engineer</h3>
+      <div className="flex justify-center flex-col items-center mt-20">
+        <h3 className="text-2xl">Hello There,</h3>
+        <img
+          className="relative mx-auto rounded-full w-80 h-80 mt-20 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+          src="../files/portfolio-pic.jpg"
+          alt="My Pic"
+        ></img>
+
+        <h1 className=" text-teal-400 text-6xl pt-20 pl-20 pb-10  md:pl-3">
+          I'm N. J. Andersen.
+        </h1>
+        <h3 className="text-2xl mb-20">Full stack web engineer</h3>
       </div>
     </section>
   );
